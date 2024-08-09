1cbbcb4df2c5e772119727bd352e13d0b173a9f7
Knoka1 <knoka1@hotmail.com>
2024-08-09 12:34:13 -0300

    fix: Atualiza postProperty para devolver null quando houver um erro

--- lib/api/postProperty.ts
+++ lib/api/postProperty.ts
@@ -1,11 +1,13 @@
 import api from "../../services/api";
 
-export const postProperty = async (data: Property): Promise<Property[]> => {
+export const postProperty = async (
+  data: Property
+): Promise<Property | null> => {
   try {
     const response = await api.post("/Condominios", data);
     return response.data;
   } catch (error) {
     console.error("Error posting properties:", error);
-    return [];
+    return null;
   }
 };
