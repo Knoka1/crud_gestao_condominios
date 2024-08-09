6b804313666a327638a625cc0746873cabc003ed
Knoka1 <knoka1@hotmail.com>
2024-08-09 16:27:17 -0300

    feat: Adiciona mais algumas cores e base ao tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,23 @@ const config = {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         pacifico: ["Pacifico", "cursive"],
       },
+      colors: {
+        "black-100": "#2B2C35",
+        "primary-blue": {
+          DEFAULT: "#2B59FF",
+          100: "#F5F8FF",
+        },
+        "secondary-orange": "#f79761",
+        "light-white": {
+          DEFAULT: "rgba(59,60,152,0.03)",
+          100: "rgba(59,60,152,0.02)",
+        },
+        grey: "#747A88",
+      },
+      backgroundImage: {
+        pattern: "url('/pattern.png')",
+        "hero-bg": "url('/hero-bg.png')",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
