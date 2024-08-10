85a67908e24d69a1613ab3444e57894252a61423
Knoka1 <knoka1@hotmail.com>
2024-08-09 21:37:33 -0300

    feat: Cria CustomFilter e Searchbar components

--- components/MenuLayout/HeaderBar.tsx
+++ components/MenuLayout/HeaderBar.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import HeaderSignIn from "../HeaderSignIn";
 import Logo from "./Logo";
+import HeaderSignIn from "./HeaderSignIn";
 
 const HeaderBar = () => {
   return (
-    <div className=" bg-white z-20">
+    <div className="bg-white z-20 md:px-2">
       <div className="flex justify-evenly md:justify-normal w-full items-center mt-2">
         <Navbar />
-        {/* Logo */}
         {/* bg-white neutraliza o hover já que queremos manter esse comportamento para outros botões shadcn */}
         <Button
           variant="ghost"
