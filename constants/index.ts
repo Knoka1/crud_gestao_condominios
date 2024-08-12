283ba6d789582eae92d5c2601d342eec658438c0
Knoka1 <knoka1@hotmail.com>
2024-08-11 23:36:32 -0300

    feat: Adiciona sheet, esqueleto Navbar.tsx e react-icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,7 @@
+export const navLinks = [
+  { label: "Novo Condomínio", route: "/property/create" },
+  { label: "Novo Condomínio", route: "/property/edit" },
+];
 export const footerLinks = [
   {
     title: "About",
