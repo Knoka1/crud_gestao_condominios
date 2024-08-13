3312fdc4720c87501e92572124ff8ba45a0b765d
Knoka1 <knoka1@hotmail.com>
2024-08-12 21:07:56 -0300

    feat: Implementa pesquisa na Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,53 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Hero from "@/components/Hero";
 import PropertyCard from "@/components/PropertyCard";
 import CustomFilter from "@/components/UiCustom/CustomFilter";
 import Searchbar from "@/components/UiCustom/Searchbar";
 import { fetchProperties } from "@/lib/api/fetchProperties";
-import React from "react";
 
-const Home = async () => {
-  let properties: Property[] = [];
-  let error: string | null = null;
-  let isDataEmpty = false;
-  try {
-    properties = await fetchProperties();
-    if (properties.length === 0) {
-      isDataEmpty = true;
-    }
-  } catch (err) {
-    error =
-      "Erro ao buscar as propriedades. Por favor, tente novamente mais tarde.";
-  }
+const Home = () => {
+  const [properties, setProperties] = useState<Property[]>([]);
+  const [filteredProperties, setFilteredProperties] = useState<Property[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isDataEmpty, setIsDataEmpty] = useState(false);
+
+  useEffect(() => {
+    const getProperties = async () => {
+      try {
+        const fetchedProperties = await fetchProperties();
+        setProperties(fetchedProperties);
+        setFilteredProperties(fetchedProperties);
+        if (fetchedProperties.length === 0) {
+          setIsDataEmpty(true);
+        }
+      } catch (err) {
+        setError(
+          "Erro ao buscar as propriedades. Por favor, tente novamente mais tarde."
+        );
+      }
+    };
+
+    getProperties();
+  }, []);
+
+  const handleSearch = (query: string) => {
+    const filtered = properties.filter((property) =>
+      property.nome.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProperties(filtered);
+  };
+
   return (
     <div>
       <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
-        {" "}
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Condomínios</h1>
           <p>Explore os nossos Condomínios</p>
         </div>
-        <div className="home__filters">
-          <Searchbar />
+        <div className="home__filters mb-3">
+          <Searchbar onSearch={handleSearch} />
           <div className="home__filter-container">
             <CustomFilter title="inicioAdministracao" />
             <CustomFilter title="unidades" />
@@ -37,38 +56,25 @@ const Home = async () => {
         {!isDataEmpty ? (
           <section>
             <div className="home__property-wrapper">
-              {properties?.map((property) => (
-                <PropertyCard property={property} />
+              {filteredProperties.map((property) => (
+                <div key={property.id} className="mb-2">
+                  <PropertyCard property={property} />
+                </div>
               ))}
             </div>
           </section>
         ) : error ? (
-          <p>{error}</p>
+          <div className="home__error-container">
+            <h2 className="text-xl text-black font-bold">{error}</h2>
+          </div>
         ) : (
           <div className="home__error-container">
             <h2 className="text-xl text-black font-bold">
               Oops, parece que não conseguimos mostrar num momento
-            </h2>{" "}
+            </h2>
           </div>
         )}
       </div>
-
-      {/* <>
-          <ul>
-            {properties.map((property) => (
-              <li key={property.id}>
-                <h2>{property.nome}</h2>
-                <p>Address: {property.endereco}</p>
-                <p>CNPJ: {property.cnpj}</p>
-                <p>Units: {property.quantidadeUnidades}</p>
-                <p>
-                  Start Date:{" "}
-                  {new Date(property.inicioAdministracao).toLocaleDateString()}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </> */}
     </div>
   );
 };
--- components/UiCustom/Searchbar.tsx
+++ components/UiCustom/Searchbar.tsx
@@ -1,16 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import { FaSearch } from "react-icons/fa";
 
-const Searchbar = () => {
-  const handleSearch = () => {};
+interface ISearchbarProps {
+  onSearch: (query: string) => void;
+}
+
+const Searchbar = ({ onSearch }: ISearchbarProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(searchQuery);
+  };
+
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <div className="flex w-full items-center space-x-2">
-          <Input placeholder="Buscar..." />
+          <Input
+            placeholder="Buscar..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
           <Button className="hover:bg-cyan-500" type="submit">
             <FaSearch />
           </Button>
