00159d2d680cbfdbe8ea4f56895b7a9a4e089fed
Knoka1 <knoka1@hotmail.com>
2024-08-13 17:31:29 -0300

    feat: Estrutura base da página AllProperties.tsx

--- app/(root)/property/all/page.tsx
+++ app/(root)/property/all/page.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+"use client";
+import PropertyCard from "@/components/PropertyCard";
+import Searchbar from "@/components/UiCustom/Searchbar";
+import { fetchProperties } from "@/lib/api/fetchProperties";
+import React, { useEffect, useState } from "react";
 
 const AllProperties = () => {
-  return <div>AllProperties</div>;
+  const [properties, setProperties] = useState<Property[]>([]);
+  const [isDataEmpty, setIsDataEmpty] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filteredProperties, setFilteredProperties] = useState<Property[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getProperties = async () => {
+      try {
+        setIsLoading(true);
+        const fetchedProperties = await fetchProperties();
+        setProperties(fetchedProperties);
+        setFilteredProperties(fetchedProperties);
+        if (fetchedProperties.length === 0) {
+          setIsDataEmpty(true);
+        }
+      } catch (err) {
+        setError(
+          "Erro ao buscar as propriedades. Por favor, tente novamente mais tarde."
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getProperties();
+  }, []);
+  const handleSearch = (query: string) => {
+    const filtered = properties.filter((property) =>
+      property.nome.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProperties(filtered);
+  };
+  return (
+    <div className="">
+      <div className="home__filters mb-3 w-full">
+        <Searchbar onSearch={handleSearch} />
+      </div>
+      {isLoading ? (
+        <p>Carregando nossos Condomínios...</p>
+      ) : !isDataEmpty ? (
+        <section>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-x-3">
+            {filteredProperties.map((property) => (
+              <div key={property.id} className="mb-2">
+                <PropertyCard property={property} />
+              </div>
+            ))}
+          </div>
+        </section>
+      ) : error ? (
+        <div className="home__error-container">
+          <h2 className="text-xl text-black font-bold">{error}</h2>
+        </div>
+      ) : (
+        <div className="home__error-container">
+          <h2 className="text-xl text-black font-bold">
+            Oops, parece que não conseguimos mostrar num momento
+          </h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AllProperties;
