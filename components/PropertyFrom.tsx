8dcee6ded6e7de3ebb80c994643ac39ee2a57cb9
Knoka1 <knoka1@hotmail.com>
2024-08-15 20:36:56 -0300

    feat: Adiciona estrutura básica de validação do formulário

--- components/PropertyFrom.tsx
+++ components/PropertyFrom.tsx
@@ -15,49 +15,124 @@ import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { IoIosAddCircleOutline } from "react-icons/io";
 
-interface ProprietyFromProps {
+interface PropertyFormProps {
   type: "edit" | "new";
+  defaultValues?: Partial<z.infer<typeof formSchema>>;
 }
 
 const formSchema = z.object({
-  username: z
-    .string()
-    .min(2, { message: "Username must be at least 2 chars." }),
+  nome: z.string(),
+  endereco: z.string(),
+  cnpj: z.string(),
+  quantidadeUnidades: z
+    .number()
+    .min(1, { message: "A unidade precisa pelo menos 1 números." }),
+  inicioAdministracao: z.string({
+    message: "O condomínio precisa de uma data.",
+  }),
 });
-const PropertyFrom = ({ type }: ProprietyFromProps) => {
+
+const PropertyForm = ({ type, defaultValues }: PropertyFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      quantidadeUnidades: 1,
+      inicioAdministracao: "2024-08-14",
+      ...defaultValues, // Se o tipo for "edit", isso vai dar override nos defaults
     },
   });
+
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    const formattedValues = {
+      ...values,
+      inicioAdministracao: new Date(values.inicioAdministracao)
+        .toISOString()
+        .slice(0, 10),
+    };
+    console.log(formattedValues);
   }
+
   return (
     <section>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
           <FormField
             control={form.control}
-            name="username"
+            name="nome"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Nome</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Nome do condomínio" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button className="rounded-md bg-cyan-600" type="submit">
-            <p className="pr-1">Adicionar</p>
-            <IoIosAddCircleOutline size={20} />
-          </Button>
+          <FormField
+            control={form.control}
+            name="endereco"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Endereço</FormLabel>
+                <FormControl>
+                  <Input placeholder="Av Borba Gato, 15" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-wrap space-x-2 my-2">
+            <FormField
+              control={form.control}
+              name="cnpj"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>CNPJ</FormLabel>
+                  <FormControl>
+                    <Input placeholder="123.321.451-99" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="quantidadeUnidades"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Unidades</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      {...field}
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex items-center my-2">
+            <FormField
+              control={form.control}
+              name="inicioAdministracao"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Início da Administração</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="rounded-md bg-cyan-600 ml-24 mt-8" type="submit">
+              <p className="pr-1">Adicionar</p>
+              <IoIosAddCircleOutline size={20} />
+            </Button>
+          </div>
         </form>
       </Form>
       {/* <header className="flex flex-col gap-5 md:gap-8"></header> */}
@@ -65,4 +140,4 @@ const PropertyFrom = ({ type }: ProprietyFromProps) => {
   );
 };
 
-export default PropertyFrom;
+export default PropertyForm;
