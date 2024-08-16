f5fe5d3ba4d8f9133ed211b1c26309aac66c108e
Knoka1 <knoka1@hotmail.com>
2024-08-15 23:37:30 -0300

    feat: Cria a página de PropertyPage com o PropertyForm

--- app/(root)/property/all/edit/[id]/page.tsx
+++ app/(root)/property/all/edit/[id]/page.tsx
@@ -2,6 +2,11 @@
 import React, { useEffect, useState } from "react";
 import { fetchPropertyById } from "@/lib/api/fetchPropertyById";
 import { deletePropertyById } from "@/lib/api/deletePropertyById";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import PropertyForm from "@/components/PropertyForm";
+import { putPropertyById } from "@/lib/api/putPropetyById";
+import WarningAlert from "@/components/Alerts/WarningAlert";
 
 interface IPropertyPageProps {
   params: { id: string };
@@ -11,12 +16,14 @@ const PropertyPage = ({ params }: IPropertyPageProps) => {
   const [property, setProperty] = useState<Property | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const { toast } = useToast();
 
   useEffect(() => {
     const fetchProperty = async () => {
       const id = params?.id;
       try {
-        const fetchedProperty = await fetchPropertyById(id);
+        const fetchedProperty = await fetchPropertyById(Number(id));
         if (fetchedProperty === null) {
           setError("Condomínio não encontrado.");
         } else {
@@ -33,28 +40,47 @@ const PropertyPage = ({ params }: IPropertyPageProps) => {
 
     fetchProperty();
   }, []);
-  const handleDelete = async (id: number) => {
-    await deletePropertyById(id);
+  const handleDelete = async () => {
+    await deletePropertyById(Number(params.id));
   };
-
+  const handleEditProperty = async (property: Property) => {
+    property.id = Number(params.id);
+    try {
+      const response = await putPropertyById(Number(params.id), property);
+      if (response === null) {
+        return toast({
+          variant: "destructive",
+          title: "Não foi editar o condomínio",
+          description: `Por favor, tente novamente.`,
+        });
+      } else {
+        router.push("/property/all");
+        return toast({
+          title: "Condomínio editado com sucesso",
+          description: `O condomínio ${property.nome} foi editado.`,
+        });
+      }
+    } catch (err) {
+      console.log("Erro ao enviar os dados. Erro:", err);
+    }
+  };
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
   if (!property) return <p>Property not found.</p>;
 
   return (
-    <div>
-      <h1>{property.nome}</h1>
-      <p>Address: {property.endereco}</p>
-      <p>CNPJ: {property.cnpj}</p>
-      <p>Units: {property.quantidadeUnidades}</p>
-      <p>
-        Start Date:{" "}
-        {new Date(property.inicioAdministracao).toLocaleDateString()}
-      </p>
-      <button onClick={() => handleDelete(Number(params?.id))}>
-        Delete Property
-      </button>
-    </div>
+    <section className="flex flex-col">
+      <h1 className="text-4xl font-extrabold">Editar Condomínio</h1>
+      <p className="my-2 mb-4">Altualize as informações do Condomínio</p>
+      <PropertyForm
+        type="edit"
+        onSubmit={handleEditProperty}
+        defaultValues={property}
+      />
+      <div className="flex mt-6">
+        <WarningAlert handleDelete={handleDelete} />
+      </div>
+    </section>
   );
 };
 
